5c49330c54f023c5ae1783e133c804a91ff041af
Lou Huang <lou@codeforamerica.org>
2013-12-05 22:50:52 -0800

    Fix for foundation topbar. Also pushed to foundation master.

--- js/vendor/foundation.js
+++ js/vendor/foundation.js
@@ -3775,6 +3775,7 @@
           } else {
             topbar.addClass('fixed');
             topbar.parent().addClass('expanded');
+            $('body').addClass('f-topbar-fixed');
           }
         }
       }
